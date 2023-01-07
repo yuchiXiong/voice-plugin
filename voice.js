2937fa1ec97aca3cefb1edad1912d4d01576ce82
yuchiXiong <yuchi.xiong@foxmail.com>
2023-01-07 20:50:03 +0800

    feat: 增加一个测试指令，困呐困呐语音触发概率调整为80%，每日问候增加包包困呐困呐语音彩蛋；缓存每天的问候角色信息，一天会有有一位角色进行日常问候

--- voice.js
+++ voice.js
@@ -16,6 +16,7 @@ export class mysNews extends plugin {
       rule: [
         {
           reg: '^早上好|中午好|晚上好|晚安$',
+          // reg: '^T(早上好|中午好|晚上好|晚安)$',
           fnc: 'greet'
         },
         {
@@ -40,55 +41,93 @@ export class mysNews extends plugin {
 
   /** 语音回复 困呐困呐困呐啊啊啊啊 */
   async sleepy() {
+    if (Math.random() >= 0.8) return
+
     /** 包包：怎么还在睡啊！ */
-    const file = fs.readFileSync('./plugins/voice-plugin/audio/sleepy.wav')
+    const file = fs.readFileSync('./plugins/voice-plugin/audio/wakeUp.wav')
 
     const msg = segment.record(file)
     await this.reply(msg)
   }
 
   async greet() {
-    /** 可莉 */
-    // const url = 'https://api-static.mihoyo.com/common/blackboard/ys_obc/v1/content/info?app_sn=ys_obc&content_id=55'
+    const rand = Math.random()
+    if (this.e.msg.includes('早上好') && rand <= 0.3) {
+      console.log('[日常问候] 宴宁彩蛋语音')
 
-    /** 珊瑚宫心海 */
-    // const url = 'https://api-static.mihoyo.com/common/blackboard/ys_obc/v1/content/info?app_sn=ys_obc&content_id=2403'
+      /** 包包：困呐困呐困呐啊啊啊啊！ */
+      const file = fs.readFileSync('./plugins/voice-plugin/audio/sleepy.wav')
 
-    /** 宵宫 */
-    const url = 'https://api-static.mihoyo.com/common/blackboard/ys_obc/v1/content/info?app_sn=ys_obc&content_id=2124'
-    const headers = {
-      Referer: 'https://bbs.mihoyo.com/',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-    }
-    let response
-    try {
-      response = await fetch(url, { method: 'get', headers })
-    } catch (error) {
-      logger.error(error.toString())
-      return false
+      const msg = segment.record(file)
+      await this.reply(msg)
+      return
     }
 
-    if (!response.ok) {
-      logger.error(`[米游社接口错误][${type}] ${response.status} ${response.statusText}`)
-      return false
+    const _d = new Date()
+    const today = `${_d.getFullYear()}-${_d.getMonth() + 1}-${_d.getDay()}`
+
+    const redisKey = `VoicePlugin#greet[${today}]`
+
+    let role = await redis.get(redisKey)
+    let first = false
+
+    if (role) {
+      role = JSON.parse(role)
+    } else {
+      /** 角色wiki */
+      const allRoleWikiUrl = 'https://api-static.mihoyo.com/common/blackboard/ys_obc/v1/home/content/list?app_sn=ys_obc&channel_id=189'
+      const _res = await (await fetch(allRoleWikiUrl)).json()
+
+      const allRoleObj = _res.data.list[0].children[0].list.map(i => ({
+        id: i.content_id,
+        name: i.title
+      }))
+
+      role = allRoleObj[Math.floor(Math.random() * allRoleObj.length)]
+
+      first = true
+
+      redis.set(redisKey, JSON.stringify(role), { EX: 3600 * 24 })
     }
-    const res = await response.json()
-    const $ = load(res.data.content.contents[2].text)
 
-    const allSource = $('.obc-tmpl-character__voice-pc tr>td')
+    console.log('[日常问候] 今日角色： ' + role.name)
 
-    const allUrl = []
+    const voiceReidsKey = `VoicePlugin::${role.name}`
+    let sourceInfo = await redis.get(voiceReidsKey)
 
-    for (let i = 0; i < allSource.length / 4; i += 2) {
-      const name = load(allSource[i]).text()
-      const url = load(allSource[i + 1])('source')[0].attribs.src
-      allUrl.push({
-        name,
-        url
-      })
+    if (sourceInfo) {
+      sourceInfo = JSON.parse(sourceInfo)
+    } else {
+      const url = `https://api-static.mihoyo.com/common/blackboard/ys_obc/v1/content/info?app_sn=ys_obc&content_id=${role.id}`
+      const headers = {
+        Referer: 'https://bbs.mihoyo.com/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+      }
+      const res = await (await fetch(url, { method: 'get', headers })).json()
+
+      const $ = load(res.data.content.contents[2].text)
+
+      const allSource = $('.obc-tmpl-character__voice-pc tr>td')
+
+      sourceInfo = []
+
+      for (let i = 0; i < allSource.length / 4; i += 2) {
+        const name = load(allSource[i]).text()
+        const url = load(allSource[i + 1])('source')[0].attribs.src
+        sourceInfo.push({
+          name,
+          url
+        })
+      }
+
+      redis.set(voiceReidsKey, JSON.stringify(sourceInfo))
     }
 
-    const msg = segment.record(allUrl.find(i => i.name.includes(this.e.msg)).url)
+    if (first) {
+      await this.reply(segment.record(sourceInfo.find(i => i.name.includes('初次见面')).url))
+    }
+
+    const msg = segment.record(sourceInfo.find(i => i.name.includes(this.e.msg.split('T')[1])).url)
     await this.reply(msg)
   }
 }
